2d91846299243cffcdb044167a8d259a83fefedd
Ricardo Toucedo <toucedo77@gmail.com>
2021-03-08 16:13:55 -0100

    Added fucntion to display powerstats of the super hero generated randomly

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 window.onload = function () {
-    
-    const super_hero = document.getElementById("super_hero");
+
     const hero_btn = document.getElementById("hero_btn");
 
     hero_btn.addEventListener('click', getRandomSuperHero);
-    
+
 }
 
 function getRandomSuperHero() {
-    const url = "https://superheroapi.com/api/3755224024532374/"
-    let randomNumber = Math.floor(Math.random()*30)+1;
+    let randomNumber = Math.floor(Math.random() * 731) + 1;
 
-    fetch(`https://www.superheroapi.com/api.php/3755224024532374/${randomNumber}/image`)
+    fetch(`https://www.superheroapi.com/api.php/3755224024532374/${randomNumber}`)
         .then(resp => resp.json())
         .catch(error => {
             console.log(error.response);
         })
         .then(data => {
-            super_hero.innerHTML = `<img src="${data.url}" alt="${data.name}"/>`
+            super_hero.innerHTML = `<img src="${data.image.url}" alt="${data.name}"/>`;
+            super_hero_name.innerHTML = `${data.name}`;
+            powerstats.innerHTML = "";
+            for(let prop in data.powerstats){
+                powerstats.innerHTML += `<p>${prop} ${data.powerstats[prop]}</p>`
+            }
+
         })
-        console.log(randomNumber);
 };
